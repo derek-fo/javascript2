d5f25a1497b1b2310726fbdd17e638e7c74ac6ee
derek-fo <derek.foliveira@gmail.com>
2025-03-23 12:43:11 -0300

    aula11 js - condições

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 45
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+if (vel > 60) { //condição simples
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+else {
+    console.log('Dirija com responsabilidade!')
+}
