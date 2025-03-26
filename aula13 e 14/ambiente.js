79cbcdf6e90bd5f1a6e0edb14820c3f5382cd24b
derek-fo <derek.foliveira@gmail.com>
2025-03-25 21:18:55 -0300

    repetições

---
+++ aula13 e 14/ambiente.js
@@ -0,0 +1,18 @@
+/* var c = 1
+while (c < 6) {
+    console.log('Tudo bem?')
+    c++  ->  c = c + 1 
+} */
+
+
+/* var c = 1
+do {
+    console.log('Tudo bem?')
+    c++  c = c + 1
+} while (c < 4) */
+
+console.log('Vai começar...')
+for(var c = 1; c <= 5; c++){
+    console.log(c)
+}
+console.log('Fim!')
