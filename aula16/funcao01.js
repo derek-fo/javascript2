a056dca745cdc2b5e6d5b64bf7e7c3fb3548cb6b
derek-fo <derek.foliveira@gmail.com>
2025-03-30 22:36:06 -0300

    funções

---
+++ aula16/funcao01.js
@@ -0,0 +1,15 @@
+function parimpar (n) {
+    if (n%2==0) {
+        return 'PAR!'
+    }
+    else {
+        return 'ÍMPAR!'
+    }
+}
+
+console.log(parimpar(20))
+
+
+
+
+
---
+++ aula16/funcao02.js
@@ -0,0 +1,9 @@
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+console.log(soma(3))
+
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+console.log(soma(3, 7))
