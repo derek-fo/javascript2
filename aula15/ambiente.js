2512865ad81e1c80cc4cc62034ec4165616800a4
derek-fo <derek.foliveira@gmail.com>
2025-03-30 21:13:42 -0300

    repetições compostas

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+var num = [2, 7 ,9, 3, 1, 5] 
+num.push(4)
+
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`Nosso vetor em ordem crescente é ${num.sort()}`)
---
+++ aula15/ambiente2.js
@@ -0,0 +1,11 @@
+let valores = [2, 7 ,9, 3, 1, 5] 
+
+/* for(let pos=0; pos < valores.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+} */
+
+for (let pos in valores){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+
+
